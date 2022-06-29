5105d36376d2614e96a2ad9d8f14d89326b5c1aa
anvaka <anvaka@gmail.com>
2022-06-28 22:16:44 -0700

    use main branch

--- deploy.sh
+++ deploy.sh
@@ -6,5 +6,5 @@ git init
 git add .
 git commit -m 'push to gh-pages'
 ## Change the line below to deploy to your gh-pages
-git push --force git@github.com:anvaka/map-of-reddit.git master:gh-pages
+git push --force git@github.com:anvaka/map-of-reddit.git main:gh-pages
 cd ../
