fad33d20e16b9896f4a456567676a694fabe3c4c
anvaka <anvaka@gmail.com>
2023-04-15 17:49:59 -0700

    Allow version switch

--- src/appState.js
+++ src/appState.js
@@ -1,17 +1,33 @@
 let qs = require('query-state')
 let state = qs.instance({}, {useSearch: true})
 let pendingSave;
+let versionToLink = {
+  1: 'https://anvaka.github.io/map-of-reddit-data/graph.svg',
+  2: 'https://anvaka.github.io/map-of-reddit-data/v2/graph.svg',
+  // 3: 'http://127.0.0.1:8080/graph.svg',
+};
+
+let lastVersion = 1;
+let latestGraphLink = versionToLink[lastVersion];
+// we should ignore camera position if user doesn't have graph version in the link
+// it means they have stored previous link, and we have changed the graph, so their
+// link wouldn't work correctly.
+let versionMissing = state.get('v') === undefined;
+let graphDefined = state.get('graph') !== undefined;
 
 export default {
   query: state.get('q') || '',
   userTypedSomething: false,
 
   getFilePath() {
-    return state.get('graph') || 'https://anvaka.github.io/map-of-reddit-data/graph.svg';
+    const version = state.get('v')
+    return state.get('graph') || versionToLink[version] || latestGraphLink;
   },
   saveQuery(newQuery) {
+    // make sure we store the version in all new calls:
     if (newQuery) {
-      state.set({q: newQuery});
+      const newState = {q: newQuery};
+      state.set(newState);
     } else {
       state.unset('q');
     }
@@ -20,6 +36,7 @@ export default {
     return state;
   },
   getCameraPosition() {
+    if (versionMissing && !graphDefined) return;
     let coordinates = ['x', 'y', 'z'].map(name => {
       let v = Number.parseFloat(state.get(name));
       if (Number.isFinite(v)) return v;
@@ -37,6 +54,10 @@ export default {
       let dc = scene.getDrawContext();
       let [x, y, z] = dc.view.position;
       let position = {x, y, z}
+
+      if (versionMissing && !graphDefined) {
+        position.v = lastVersion;
+      }
       state.set(position);
     }, 300);
   }
